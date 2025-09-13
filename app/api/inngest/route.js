da07394424dc78a55016545f4cfe1d17029ac189
Ayush Prem <ayushpremrocks@gmail.com>
2025-09-14 04:08:57 +0530

    remove comment in route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,8 +1,7 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { syncUserCreation } from "@/inngest/function";
+import { syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/inngest/function";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
